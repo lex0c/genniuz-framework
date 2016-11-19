e7cea6fce2fd22588a2b4408b6ddd99755963451
Léo Castro <leonardo_carvalho@outlook.com>
2016-11-19 00:59:27 -0200

    Tasks for gulp

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
+var sourcemaps = require('gulp-sourcemaps');
+
+
+gulp.task('taskNameHere', function(){
+    //Code here...
+});
+
+
+
+gulp.task('watch', function(){});
