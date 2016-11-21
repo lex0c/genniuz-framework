2720d04bd7a840095f7e929fb1bd9d7545cc4f61
Léo Castro <leonardo_carvalho@outlook.com>
2016-11-20 22:38:07 -0200

    Refactored

--- tests/config/config-access-data.php
+++ tests/config/config-access-data.php
@@ -1,48 +1,46 @@
 <?php
 
-require_once (__DIR__ . "/../../vendor/autoload.php");
-
-use \Config\App;
-use \Config\Database;
+$app = require_once (__DIR__ . '/../../config/app.php');
+$db = require_once (__DIR__ . '/../../config/database.php');
 
 echo "<b>----- App -----</b>";
 echo "<br>";
-var_dump(App::get('env'));
+var_dump($app['env']);
 echo "<br>";
-var_dump(App::get('debug'));
+var_dump($app['debug']);
 echo "<br>";
-var_dump(App::get('url'));
+var_dump($app['url']);
 echo "<br>";
-var_dump(App::get('root'));
+var_dump($app['root']);
 echo "<br>";
-var_dump(App::get('charset'));
+var_dump($app['charset']);
 echo "<br>";
-var_dump(App::get('lang'));
+var_dump($app['lang']);
 
 echo "<br>";
 echo "<br>";
 
 echo "<b>----- Database -----</b>";
 echo "<br>";
-var_dump(Database::get('name'));
+var_dump($db['name']);
 echo "<br>";
-var_dump(Database::get('prefix'));
+var_dump($db['prefix']);
 echo "<br>";
-var_dump(Database::get('host'));
+var_dump($db['host']);
 echo "<br>";
-var_dump(Database::get('port'));
+var_dump($db['port']);
 echo "<br>";
-var_dump(Database::get('driver'));
+var_dump($db['driver']);
 echo "<br>";
-var_dump(Database::get('username'));
+var_dump($db['username']);
 echo "<br>";
-var_dump(Database::get('password'));
+var_dump($db['password']);
 echo "<br>";
-var_dump(Database::get('fetch'));
+var_dump($db['fetch']);
 echo "<br>";
-var_dump(Database::get('charset'));
+var_dump($db['charset']);
 echo "<br>";
-var_dump(Database::get('engine'));
+var_dump($db['engine']);
 
 echo "<br>";
 echo "<br>";
