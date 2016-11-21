c7430567ed883e8764d060544e54a58c19ef8b83
Léo Castro <leonardo_carvalho@outlook.com>
2016-11-20 23:46:04 -0200

    Refactored and commented

--- system/interfaces/LoaderInterface.php
+++ system/interfaces/LoaderInterface.php
@@ -1,13 +1,21 @@
 <?php //namespace System\Interfaces;
 
-/**
- * 
+/*
+ ===========================================================================
+ = Interface para padronização dos carregadores
+ ===========================================================================
+ =
+ = Define um padrão para criação das classes responsaveis por carregamento
+ = de arquivos de configurações da aplicação.
+ = 
  */
 
 Interface LoaderInterface
 {
     /**
-     *  
+     * Carregador
+     * @param string $index [key]
+     * @param array $data [opcional]
      */
     public static function loader($index, array $data);
     
