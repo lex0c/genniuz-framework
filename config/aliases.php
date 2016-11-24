fc3969c788f8857cd5fe251707a5c2d6804bcda5
Léo Castro <leonardo_carvalho@outlook.com>
2016-11-24 14:22:12 -0200

    Renomed this package 'config/' for 'configs/'

--- config/aliases.php
+++
@@ -1,7 +0,0 @@
-<?php return [
-    'modules' => __DIR__ . '/../system/modules.php',
-    'autoload' => __DIR__ . '/../bootstrap/_autoload/autoload.php',
-    
-    'interfaces/Loader' => __DIR__ . '/../system/interfaces/LoaderInterface.php'
-    
-];
