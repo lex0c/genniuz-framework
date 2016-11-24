8348c3ab0346cff0fb5f7d051ef98172218f6924
Léo Castro <leonardo_carvalho@outlook.com>
2016-11-24 20:26:52 -0200

    Runnable interface

---
+++ system/interfaces/IRunnable.php
@@ -0,0 +1,35 @@
+<?php namespace System\Interfaces;
+
+/*
+ ===========================================================================
+ = Interface para padronização dos carregadores
+ ===========================================================================
+ =
+ = Define um padrão para criação das classes responsaveis por carregamento
+ = de componentes e configurações da aplicação.
+ = 
+ */
+
+Interface IRunnable
+{
+    /**
+     * Inicializa
+     * @param array
+     * @return boolean
+     */
+    public static function run(array $data);
+     
+    /**
+     * Trava o estado de um recursos 
+     * @param array
+     * @return boolean
+     */
+    public static function sleep(array $data);
+
+    /**
+     * Limpa e libera memória
+     * @return boolean
+     */
+    public static function destroy();
+    
+}
