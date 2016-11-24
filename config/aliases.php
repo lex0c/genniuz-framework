479e0fcdb65ab55760c381bd9c092d70792c0519
Léo Castro <leonardo_carvalho@outlook.com>
2016-11-24 01:38:26 -0200

    Aliases for practical development

---
+++ config/aliases.php
@@ -0,0 +1,7 @@
+<?php return [
+    'modules' => __DIR__ . '/../system/modules.php',
+    'autoload' => __DIR__ . '/../bootstrap/_autoload/autoload.php',
+    
+    'interfaces/Loader' => __DIR__ . '/../system/interfaces/LoaderInterface.php'
+    
+];
