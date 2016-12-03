2d94322435b84cc184bdfa3de77bac0ce4bae7b5
Léo Castro <leonardo_carvalho@outlook.com>
2016-12-03 15:43:45 -0200

    Initial commit

---
+++ system/kernel/timer.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * System Timer
+ * Core functions for including other source files, loading models, etc...
+ * 
+ * @link https://github.com/lleocastro/genniuz-framework/
+ * @license (MIT) https://github.com/lleocastro/genniuz-framework/blob/master/LICENSE
+ * @author Leonardo Carvalho <leonardo_carvalho@outlook.com>
+ * @package \System\Kernel;
+ * @copyright 2016 
+ * @version 1.0.0
+ */
+
+/**
+ * Basic defines for timing functions.
+ */
+define('SECOND', 1);
+define('MINUTE', 60);
+define('HOUR', 3600);
+define('DAY', 86400);
+define('WEEK', 604800);
+define('MONTH', 2592000);
+define('YEAR', 31536000);
+
+
+
+
+
+//...
